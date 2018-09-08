71c276809e19cade63e5995740fa4938ac27a348
Brian Kichler <brian.kichler@gmail.com>
2018-09-08 14:31:11 -0700

    Working recursive build_tree method

--- lib/binary_tree_sort.rb
+++ lib/binary_tree_sort.rb
@@ -10,18 +10,36 @@ class BinaryTree
 end
 
 class BTreeSort
-  attr_accessor :array
+  attr_accessor :array, :tree, :current_node
 
   def initialize(array)
     @array = array
     @tree = BinaryTree.new(@array.shift, nil, nil)
+    @top_node = @tree
+    @current_node = nil
   end
 
-  def self.build_tree
-    start = @tree.payload
-    
+  def build_tree(node)
+    if @array.first < node.payload
+      if node.left.nil?
+        node.left = BinaryTree.new(@array.shift, nil, nil)
+        build_tree(@top_node)
+      else
+        @current_node = node.left
+        build_tree(@current_node)
+      end
+    elsif @array.first > node.payload
+      if node.right.nil?
+        node.right = BinaryTree.new(@array.shift, nil, nil)
+        build_tree(@top_node)
+      else
+        @current_node = node.right
+        build_tree(@current_node)
+      end
+    end
   end
 end
 
-tree = BTreeSort.new([7, 4, 9, 1, 6, 14, 10]).build_tree
-puts tree.inspect
+
+tree = BTreeSort.new([7, 4, 9, 1, 6, 14, 10])
+tree.build_tree(tree.tree)
