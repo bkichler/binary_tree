0f8ef0aa187eff8085aafc0096555db2d07d055a
Brian Kichler <brian.kichler@gmail.com>
2018-09-09 16:05:04 -0700

    Working binary tree sort algorithm

--- lib/binary_tree_sort.rb
+++ lib/binary_tree_sort.rb
@@ -10,19 +10,21 @@ class BinaryTree
 end
 
 class BTreeSort
-  attr_accessor :array, :tree, :current_node
+  attr_accessor :array, :sort_ary, :tree, :current_parent
 
   def initialize(array)
     @array = array
     @tree = BinaryTree.new(@array.shift, nil, nil)
+    @sort_ary = []
     @top_node = @tree
-    @current_node = nil
+    @current_parent = nil
   end
 
   def build_tree(node)
+    return @tree if @array.empty?
     if @array.first < node.payload
       if node.left.nil?
-        node.left = BinaryTree.new(@array.shift, nil, nil)
+        node.left = BinaryTree.new(@array.shift, nil, nil)   
         build_tree(@top_node)
       else
         @current_node = node.left
@@ -38,8 +40,34 @@ class BTreeSort
       end
     end
   end
+
+  def tree_sort(node)
+    return @sort_ary if !defined?(node.left)
+
+    if @top_node.left.nil?
+      @sort_ary.push(@top_node.payload)
+      @top_node = @top_node.right
+      tree_sort(@top_node)
+    elsif !node.left.nil?
+      @current_parent = node
+      tree_sort(node.left)
+    elsif node.left.nil?
+      @sort_ary.push(node.payload)
+      if !node.right.nil?
+        tree_sort(node.right)
+      elsif node.right.nil?
+        @current_parent.left = nil
+        tree_sort(@top_node)
+      end
+    end
+  end
 end
 
 
 tree = BTreeSort.new([7, 4, 9, 1, 6, 14, 10])
 tree.build_tree(tree.tree)
+puts tree.tree.inspect
+tree.tree_sort(tree.tree)
+puts tree.sort_ary.inspect
+puts tree.current_parent.inspect
+puts tree.tree.inspect
--- spec/btree_spec.rb
+++ spec/btree_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe BTreeSort, type: :model do
     it 'should order the array' do
       expected = [1, 4, 6, 7, 9, 10, 14]
       array = [7, 4, 9, 1, 6, 14, 10]
-      expect(BTreeSort.sort(array)).to eq expected
+      expect(BTreeSort.tree_sort(array)).to eq expected
     end
   end
 end
